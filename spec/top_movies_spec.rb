d21d6a2f55fe0f1814f111fd6dc56826bbe7fd1c
Jason Mei <jason.mei403@gmail.com>
2018-09-03 20:27:27 +0000

    first commit

---
+++ spec/top_movies_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe TopMovies do
+  it "has a version number" do
+    expect(TopMovies::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
