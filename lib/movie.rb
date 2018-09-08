bf2e5fa6a4be7a784d611b569a2df330f89f949c
Jason Mei <jason.mei403@gmail.com>
2018-09-08 20:42:06 +0000

    added 'nokogiri' and 'pry' to Gemfile

--- lib/movie.rb
+++ lib/movie.rb
@@ -1,2 +1,13 @@
 # require_relative "./version"
 # require_relative "./cli"
+
+class Movie
+  attr_accessor :title, :genre, :director, :writers, :main_cast, :runtime, :critic_score, :audience_score
+  @@all = []
+
+  def initialize
+    
+  end
+
+
+end
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,3 +1,16 @@
+require 'nokogiri'
+require 'open-uri'
+
 class Scraper
+
+  def self.scrape_list
+    url = Nokogiri::HTML(open("https://www.rottentomatoes.com/browse/box-office/?rank_id=0&country=us"))
+    movies = []
+
+    url.css("tbody").each do |movie|
+      movies << movie.css("a").text
+    end
+    movies
+  end
 
 end
