c1df3d87a297e63617c5710218f6da1c0a9b6abc
Jason Mei <jason.mei403@gmail.com>
2018-09-08 02:04:32 +0000

    Renamed bin file and created fake data for CLI.

--- lib/top_movies/cli.rb
+++ lib/top_movies/cli.rb
@@ -1,5 +1,27 @@
 class TopMovies::CLI
-  def call
-    puts "Welcome!"
+
+  def run
+    puts "Welcome! Please enter a year to see its top 10 movies (2014 - 2018):"
+    year = gets.strip.to_i
+    if !year.between?(2014, 2018)
+      puts "Invalid year. Please enter years between 2014 - 2018."
+    else
+      list_movies
+    end
+  end
+
+  def list_movies
+    puts <<-DOC
+    1. Avengers Infinity War
+    2. Black Panther
+    3. Captain America - Civil War
+    4. Spider-Man Homecoming
+    5. Marvel's The Avengers
+    6. Captain America - Winter Soldier
+    7. Iron Man
+    8. Captain America - The First Avenger
+    9. Thor
+    10. Guardians of the Galaxy
+    DOC
   end
 end
