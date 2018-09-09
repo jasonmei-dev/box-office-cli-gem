9513b7ce166fc56538f5e9b194212a18881ca98f
Jason Mei <jason.mei403@gmail.com>
2018-09-09 13:32:38 +0000

    Modified scraper.rb and cli.rb to scrape movie list from RT

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,3 +1,6 @@
+require_relative '../lib/scraper.rb'
+require_relative '../lib/movie.rb'
+
 require 'nokogiri'
 require 'open-uri'
 require 'pry'
@@ -11,18 +14,22 @@ class CommandLineInterface
 
   def list_movies
     puts "Last Weekend's Box Office Top Ten:"
-    puts <<-DOC
-    1. Crazy Rich Asians, $22.1M
-    2. The Meg, $10.5M
-    3. Mission: Impossible - Fallout, $7.0M
-    4. Searching, $6.1M
-    5. Operation Finale, $6.0M
-    6. Christopher Robin, $5.2M
-    7. Alpha, $4.5M
-    8. The Happytime Murders, $4.4M
-    9. BlacKKKlansman, $4.2M
-    10. Mile 22, $3.6M
-    DOC
+    # puts <<-DOC
+    # 1. Crazy Rich Asians, $22.1M
+    # 2. The Meg, $10.5M
+    # 3. Mission: Impossible - Fallout, $7.0M
+    # 4. Searching, $6.1M
+    # 5. Operation Finale, $6.0M
+    # 6. Christopher Robin, $5.2M
+    # 7. Alpha, $4.5M
+    # 8. The Happytime Murders, $4.4M
+    # 9. BlacKKKlansman, $4.2M
+    # 10. Mile 22, $3.6M
+    # DOC
+    @movies = Scraper.scrape_movie_list
+    @movies.each_with_index do |movie, i|
+      puts "#{i + 1}. #{movie}" 
+    end
   end
 
   def menu
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,12 +3,12 @@ require 'open-uri'
 
 class Scraper
 
-  def self.scrape_list
+  def self.scrape_movie_list
     url = Nokogiri::HTML(open("https://www.rottentomatoes.com/browse/box-office/?rank_id=0&country=us"))
     movies = []
 
-    url.css("tbody").each do |movie|
-      movies << movie.css("a").text
+    url.css("table td a").each do |movie|
+      movies << movie.text if movies.length < 10
     end
     movies
   end
