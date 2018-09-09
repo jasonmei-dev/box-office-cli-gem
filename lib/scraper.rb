ed04ba3d433d926a01c4eea94b90802ca87268ba
Jason Mei <jason.mei403@gmail.com>
2018-09-09 15:45:23 +0000

    Scraper now only includes movies from box office chart.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,9 +5,11 @@ class Scraper
 
   def self.scrape_movie_list
     url = Nokogiri::HTML(open("https://www.rottentomatoes.com/browse/box-office/?rank_id=0&country=us"))
+    # binding.pry
     movies = []
 
-    url.css("table td a").each do |movie|
+    url.css("table td.left a").each do |movie|
+      binding.pry
       movies << movie.text if movies.length < 10
     end
     movies
